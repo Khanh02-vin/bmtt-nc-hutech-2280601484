73ea3fc9e311a9ef062fd2c245933f902d09a7c2
Khanh02-vin <takaokaginji@gmail.com>
2025-03-13 10:09:06 +0700

    [add] lab-02 ex01 vigenere

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, jsonify
-
+from cipher.railfence import RailFenceCipher
 from cipher.caesar import CaesarCipher
 from cipher.vigenere import VigenereCipher
 app = Flask(__name__)
 caesar_cipher = CaesarCipher()
 vigenere_cipher=VigenereCipher()
-
+railfence_cipher=RailFenceCipher()
 @app.route("/api/caesar/encrypt", methods=["POST"])
 def caesar_encrypt():
     data = request.json
@@ -21,6 +21,38 @@ def caesar_decrypt():
     key = int(data.get('key', 0))
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decrypted_text})
+
+@app.route("/api/vigenere/encrypt", methods=["POST"])
+def vigenere_encrypt():
+    data = request.json
+    plain_text = data.get('plain_text', '')
+    key = int(data.get('key', 0))
+    encrypt_text = vigenere_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_message': encrypt_text})
+
+@app.route("/api/vigenere/decrypt", methods=["POST"])
+def caesar_decrypt():
+    data = request.json
+    cipher_text = data.get('cipher_text', '')
+    key = int(data.get('key', 0))
+    decrypted_text = vigenere_cipher.decrypt_text(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+@app.route("/api/railfence/encrypt", methods=["POST"])
+def encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypt_text = railfence_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_message': encrypt_text})
+
+@app.route("/api/railfence/decrypt", methods=["POST"])
+def decrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    decrypt_text = railfence_cipher.decrypt_text(plain_text, key)
+    return jsonify({'encrypted_message': decrypt_text})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
